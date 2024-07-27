8d45efd234d8525cb1850d738446df7ad8649d17
roshameow <38164553+roshameow@users.noreply.github.com>
2024-07-27 15:41:21 +0000

    Deploying to gh-pages from  @ 61e66af48b844720841274332b797887e32f05e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,139 +1,4 @@
 var store = [{
-        "title": "Edge Case: Nested and Mixed Lists",
-        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that lists within lists do not break the ordered list numbering order and list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["docs","Edge Case"],
-        "tags": ["content","css","edge case","lists","markup"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/edge%20case/edge-case-nested-and-mixed-lists/"
-      },{
-        "title": "Edge Case: Many Tags",
-        "excerpt":"This post has many tags. ","categories": ["docs","Edge Case"],
-        "tags": ["8BIT","alignment","Articles","captions","categories","chat","comments","content","css","dowork","edge case","embeds","excerpt","Fail","featured image","FTW","Fun","gallery","html","image","Jekyll","layout","link","Love","markup","Mothership","Must Read","Nailed It","Pictures","Post Formats","quote","standard","Success","Swagger","Tags","template","title","twitter","Unseen","video","YouTube"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/edge%20case/edge-case-many-tags/"
-      },{
-        "title": "Edge Case: Many Categories",
-        "excerpt":"This post has many categories. ","categories": ["docs","aciform","antiquarianism","arrangement","asmodeus","broder","buying","championship","chastening","disinclination","disinfection"],
-        "tags": ["categories","edge case"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/aciform/antiquarianism/arrangement/asmodeus/broder/buying/championship/chastening/disinclination/disinfection/edge-case-many-categories/"
-      },{
-        "title": "Edge Case: No Body Content",
-        "excerpt":"","categories": ["docs","Edge Case"],
-        "tags": ["content","edge case","layout"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/edge%20case/edge-case-no-body-content/"
-      },{
-        "title": "Edge Case No Yaml Title",
-        "excerpt":"This post has no title specified in the YAML Front Matter. Jekyll should auto-generate a title from the filename. For example 2009-09-05-edge-case-no-yaml-title.md becomes Edge Case No Yaml Title. ","categories": ["docs","Edge Case"],
-        "tags": ["edge case","layout","title"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/edge%20case/edge-case-no-yaml-title/"
-      },{
-        "title": "Antidisestablishmentarianism",
-        "excerpt":"This post title has a long word that could potentially overflow the content area. A few things to check for: Non-breaking text in the title should have no adverse effects on layout or functionality. Check the browser window / tab title.The following CSS property will help you support non-breaking text....","categories": ["docs","Edge Case"],
-        "tags": ["content","css","edge case","html","layout","title"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/edge%20case/edge-case-title-should-not-overflow-the-content-area/"
-      },{
-        "title": "Suspicio? Bene ... tunc ibimus? Quis uh ... CONEXUS locus his diebus? Quisque semper aliquid videtur, in volutpat mauris. Nolo enim dicere. Vobis neque ab aliis. Ego feci memetipsum explicans. Gus mortuus est. Lorem opus habeo. Jackson Isai? Tu quoque ... A te quidem a ante. Vos scitis quod blinking res Ive 'been vocans super vos? Et conteram illud, et conteram hoc. Maledicant druggie excors. Iam hoc tu facere conatus sum ad te in omni tempore? Ludum mutavit. Verbum est ex. Et ... sunt occid",
-        "excerpt":"Check for long titles and how they might break layouts. ","categories": ["docs","Edge Case"],
-        "tags": ["content","css","edge case","html","layout","title"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/edge%20case/edge-case-very-long-title/"
-      },{
-        "title": "Post: Modified Date",
-        "excerpt":"This post has been updated and should show a modified date if last_modified_at is used in the layout. Plugins like jekyll-sitemap use this field to add a &lt;lastmod&gt; tag your sitemap.xml. ","categories": ["docs","Post Formats"],
-        "tags": ["Post Formats","readability","standard"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/post%20formats/post-modified/"
-      },{
-        "title": "Post: Standard",
-        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["docs","Post Formats"],
-        "tags": ["Post Formats","readability","standard"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/post%20formats/post-standard/"
-      },{
-        "title": "Post: Quote",
-        "excerpt":"  Only one thing is impossible for God: To find any sense in any copyright law on the planet.   Mark Twain ","categories": ["docs","Post Formats"],
-        "tags": ["Post Formats","quote"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/post%20formats/post-quote/"
-      },{
-        "title": "Post: Link",
-        "excerpt":"This theme supports link posts, made famous by John Gruber. To use, just add link: http://url-you-want-linked to the post’s YAML front matter and you’re done.   And this is how a quote looks. Some link can also be shown. ","categories": ["docs","Post Formats"],
-        "tags": ["link","Post Formats"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/post%20formats/post-link/"
-      },{
-        "title": "Post: Video (YouTube)",
-        "excerpt":"This post tests YouTube video embeds. Simply use the responsive-embed helper include like so: {% include responsive-embed url=\"https://www.youtube.com/watch?v=-PVofD2A9t8\" ratio=\"16:9\" %}Or wrap embeds with a &lt;div&gt; element and the appropriate classes: &lt;!-- 21:9 aspect ratio --&gt;&lt;div class=\"responsive-embed responsive-embed-21by9\"&gt; &lt;iframe class=\"responsive-embed-item\" src=\"...\"&gt;&lt;/iframe&gt;&lt;/div&gt;&lt;!-- 16:9 aspect ratio --&gt;&lt;div class=\"responsive-embed responsive-embed-16by9\"&gt; &lt;iframe class=\"responsive-embed-item\" src=\"...\"&gt;&lt;/iframe&gt;&lt;/div&gt;&lt;!-- 4:3...","categories": ["docs","Post Formats"],
-        "tags": ["Post Formats"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/post%20formats/post-video-youtube/"
-      },{
-        "title": "Post: Twitter Embed",
-        "excerpt":"Oh I dunno. It&#39;s probably been over 15 years since I smudged out a face with a pencil and kneaded eraser. #WIP #Sktchy pic.twitter.com/PwqbMddyVK &mdash; Michael Rose (@mmistakes) February 1, 2017This post tests Twitter Embeds. ","categories": ["docs","Media"],
-        "tags": ["content","embeds","media","twitter"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/media/post-twitter-embeds/"
-      },{
-        "title": "Layout: Post with Table Of Contents",
-        "excerpt":"Enable table of contents on post or page by adding {% include toc %} where you’d like it to appear. Table of Contents HTML Elements Below is are some HTML elements. Check the source code to see the many embedded elements within paragraphs. Body text Lorem ipsum dolor sit amet,...","categories": ["docs","Layout"],
-        "tags": ["table of contents"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/layout/layout-table-of-contents/"
-      },{
-        "title": "Layout: Author Override",
-        "excerpt":"Sites that may have content authored from various individuals can be accommodated by using data files. To attribute an author to a post or page that is different from the site author specified in _config.yml: Step 1. Create _data/authors.yml and add authors using the following format. Anything variables found under...","categories": ["docs"],
-        "tags": [],
-        "url": "https://roshameow.github.io//personal_homepage/docs/layout-author-override/"
-      },{
-        "title": "Layout: Excerpt (Defined)",
-        "excerpt":"This is the start of the post content. This paragraph should be absent from an index page where post.excerpt is shown. ","categories": ["docs","Layout","Uncategorized"],
-        "tags": ["content","excerpt","layout"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/layout/uncategorized/layout-excerpt-defined/"
-      },{
-        "title": "Layout: Excerpt (Generated with Separator Tag)",
-        "excerpt":"This is the post content. Archive-index pages should display an auto-generated excerpt of all the content preceding the excerpt_separator, as defined in the YAML Front Matter or globally in _config.yml. Be sure to test the formatting of the auto-generated excerpt, to ensure that it doesn’t create any layout problems. Lorem...","categories": ["docs","Layout","Uncategorized"],
-        "tags": ["content","excerpt","layout"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/layout/uncategorized/layout-excerpt-generated/"
-      },{
-        "title": "Layout: Hero Image",
-        "excerpt":"This post should display a large hero image at the top of a page. This post tests a horizontal image using the following YAML Front Matter: image: path: /images/eder-oliveira-180877.jpgHero images can also be assigned more succinctly when thumbnail or caption are not used. image: /images/eder-oliveira-180877.jpgTall images will push content down...","categories": ["docs","Layout"],
-        "tags": ["content","image","layout"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/layout/layout-hero-image/"
-      },{
-        "title": "Markup: Text Readability Test",
-        "excerpt":"Portland in shoreditch Vice, labore typewriter pariatur hoodie fap sartorial Austin. Pinterest literally occupy Schlitz forage. Odio ad blue bottle vinyl, 90’s narwhal commodo bitters pour-over nostrud. Ugh est hashtag in, fingerstache adipisicing laboris esse Pinterest shabby chic Portland. Shoreditch bicycle rights anim, flexitarian laboris put a bird on it...","categories": ["docs"],
-        "tags": ["sample post","readability","test"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/markup-text-readability/"
-      },{
-        "title": "Markup: Title *with* **Markdown**",
-        "excerpt":"Using Markdown in the title should have no adverse effect on the layout or functionality. page.title example: title: \"Markup: Title *with* **Markdown**\"\"","categories": ["docs","Markdown"],
-        "tags": ["css","html","title"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/markdown/markup-title-with-markdown/"
-      },{
-        "title": "Markup: Title with Special&nbsp;---&nbsp;Characters",
-        "excerpt":"Putting special characters in the title should have no adverse effect on the layout or functionality. The title above has none-breaking spaces before and after the m-dash. &amp;nbsp;---&amp;nbsp;Latin Character Tests This is a test to see if the fonts used in this theme support basic Latin characters. ! &#8220; #...","categories": ["docs","Markup"],
-        "tags": ["html","markup","post","title"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/markup/markup-title-with-special-characters/"
-      },{
-        "title": "Markup: Text Alignment and Transformations",
-        "excerpt":"Sample text to demonstrate alignment and transformation classes. Easily realign text with alignment classes via HTML: &lt;p class=\"text-left\"&gt;Left aligned text.&lt;/p&gt;&lt;p class=\"text-center\"&gt;Center aligned text.&lt;/p&gt;&lt;p class=\"text-right\"&gt;Right aligned text.&lt;/p&gt;&lt;p class=\"text-justify\"&gt;Justified text.&lt;/p&gt;&lt;p class=\"text-nowrap\"&gt;No wrap text.&lt;/p&gt;Or with Kramdown and inline attribute lists: Left aligned text.{: .text-left}Center aligned text.{: .text-center}Right aligned text.{: .text-right}Justified text.{: .text-justify}No wrap...","categories": ["docs","Markup"],
-        "tags": ["alignment","content","css","markup"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/markup/markup-text-alignment/"
-      },{
-        "title": "Markup: Image Alignment",
-        "excerpt":"The best way to demonstrate the ebb and flow of the various image positioning options is to nestle them snuggly among an ocean of words. Grab a paddle and let’s get started. Assign classes with HTML: &lt;img src=\"image.jpg\" class=\"align-left\" alt=\"\"&gt;&lt;img src=\"image.jpg\" class=\"align-center\" alt=\"\"&gt;&lt;img src=\"image.jpg\" class=\"align-right\" alt=\"\"&gt;Or use Kramdown and inline...","categories": ["docs","Markup"],
-        "tags": ["alignment","captions","content","css","image","markup"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/markup/markup-image-alignment/"
-      },{
-        "title": "Markup: HTML Elements and Formatting",
-        "excerpt":"A variety of common HTML elements to demonstrate the theme’s stylesheet and verify they have been styled appropriately. Header one Header two Header three Header four Header five Header six Blockquotes Single line blockquote: Stay hungry. Stay foolish. Multi line blockquote with a cite reference: People think focus means saying...","categories": ["docs","Markup"],
-        "tags": [],
-        "url": "https://roshameow.github.io//personal_homepage/docs/markup/markup-html-elements-and-formatting/"
-      },{
-        "title": "Markup: Syntax Highlighting",
-        "excerpt":"Syntax highlighting is a feature that displays source code, in different colors and fonts according to the category of terms. This feature facilitates writing in a structured language such as a programming language or a markup language as both structures and syntax errors are visually distinct. Highlighting does not affect...","categories": ["docs"],
-        "tags": ["code","syntax highlighting"],
-        "url": "https://roshameow.github.io//personal_homepage/docs/markup-syntax-highlighting/"
-      },{
-        "title": "MathJax Example",
-        "excerpt":"MathJax is a simple, yet powerful, way ofincluding Tex/LaTex/MathML based mathematics in HTML webpages. Usage To enable MathJax support configure your _config.xml to: Set kramdown as the Markdown parser. Enable MathJax.The version of MathJax enabled is v3. An example setting for _config.xml is shown below: markdown: kramdownmathjax: enable: true combo:...","categories": ["docs"],
-        "tags": [],
-        "url": "https://roshameow.github.io//personal_homepage/docs/mathjax-example/"
-      },{
-        "title": "Hidden Post",
-        "excerpt":"This post has YAML Front Matter of hidden: true and should not appear in paginator.posts. ","categories": ["docs"],
-        "tags": [],
-        "url": "https://roshameow.github.io//personal_homepage/docs/hidden-post/"
-      },{
         "title": "用 jekyll+obsidian生成个人主页并部署到 github pages的工作流",
         "excerpt":"用jekyll的原因无需多说。这样配置好工作流之后，除了编写内容基本没有什么麻烦的操作(登录在线网页，换编辑器，按照统一格式改文件名之类在我看来都很麻烦。。)。 Ruby的环境管理，包管理方式 管理顺序： churby -&gt; ruby -&gt; bundle -&gt; gem -&gt; jekyll ruby： 是经常在 web 开发中使用的一种动态语言。 churby： Ruby 的环境管理工具，用于管理不同版本的 Ruby。 gem： Ruby 编写的软件包的统称，Jekyll 也是一个 gem。 bundler： 用于管理项目 gem 的工具，根据 Gemfile 管理不同版本的 gem。bundler自己本身也是gem。 如果用 Python 来类比，关系大概是这样： ruby python churby 版本号 conda create -n my_env python=版本号; conda activate my_env gem install 某个包...","categories": ["docs","WebPage"],
         "tags": ["content","jekyll","markdown","obsidian","raycast"],
@@ -425,7 +290,17 @@ var store = [{
         "url": "https://roshameow.github.io//personal_homepage/docs/math/conformal-geometry3/"
       },{
         "title": "conformal geometry学习 (四 ) -- 3-manifold",
-        "excerpt":"在局部和$\\mathbb R^3$ homeomorphic(同胚) 3-manifold和knot的本质关联 所有的3-manifold都可以由$S^3$(3 sphere) 通过Dehn Surgery得到(Lickorish-Wallace, 1960) Dehn Surgery : 从3-manifold $M$ drill 一个link的tubular neighborhood(管状邻域), 用solid torus fill 每个knot的部分. link $L$由一系列knots组成, $L=\\cup_i K_i$ 不同的link可能得到相同的3-manifold, 研究这个的是Kirby Calculus学科 .hyperbolic 3-manifold(双曲三流型) 具有恒定负曲率 Poincare half-space model($\\mathbb H^3$) 定义: $\\mathbb H^3=\\lbrace(x,y,t)\\vert\\ t&gt;0\\rbrace$, 有双曲度量 $ds^2=\\frac{dx^2+dy^2+dt^2}{t^2}$ boundary $\\partial \\mathbb H^3=\\mathbb C\\cup\\infty$ 又叫sphere at infinity 测地平面...","categories": ["docs","math"],
-        "tags": ["content","3-manifold"],
+        "excerpt":"3维流型, 在局部和$\\mathbb R^3$ homeomorphic(同胚) 3-manifold和knot的本质关联 所有的3-manifold都可以由$S^3$(3 sphere) 通过Dehn Surgery得到(Lickorish-Wallace, 1960) Dehn Surgery : 从3-manifold $M$ drill 一个link的tubular neighborhood(管状邻域), 用solid torus fill 每个knot的部分. link $L$由一系列knots组成, $L=\\cup_i K_i$ 不同的link可能得到相同的3-manifold, 研究这个的是Kirby Calculus学科 .hyperbolic 3-manifold(双曲三流型) 具有恒定负曲率 Poincare half-space model($\\mathbb H^3$) 定义: $\\mathbb H^3=\\lbrace(x,y,t)\\vert\\ t&gt;0\\rbrace$, 有双曲度量 $ds^2=\\frac{dx^2+dy^2+dt^2}{t^2}$ boundary $\\partial \\mathbb H^3=\\mathbb C\\cup\\infty$ 又叫sphere at infinity...","categories": ["docs","math"],
+        "tags": ["content","3-manifold","knot","hyperbolic_geometry"],
         "url": "https://roshameow.github.io//personal_homepage/docs/math/conformal-geometry4/"
+      },{
+        "title": "conformal geometry学习 (五 ) -- homology group",
+        "excerpt":"曲面的离散表示 曲面可以离散化用Simplicial Complex表示 曲面 $\\Sigma$ 上的triangulation(三角剖分) $T$ 每个三角形边的方向沿曲面法向的逆时针 每个边有两条相互反向的half-edge simplex-&gt;chain-&gt;boundary 单纯复形(Simplicial Complex) $\\Sigma=\\cup \\sigma$ 是由simplices(单型) 粘合而成 “粘合”条件: 1. simplex $\\sigma\\subset \\Sigma$ -&gt; $\\sigma$ 的facets $\\tau\\subset \\Sigma$ 2. $\\sigma_1,\\sigma_2\\subset \\Sigma$, $\\sigma_1\\cap \\sigma_2=\\tau\\ne \\emptyset$ 是两个simplex的交 -&gt; $\\tau\\subset \\sigma_1$, $\\tau\\subset \\sigma_2$ 是一个common facet Simplex $\\sigma$ 是$\\mathbb R^n$ 中的点集的minimal convex hull(0: simplex 是点, 1-simplex:...","categories": ["docs","math"],
+        "tags": ["content"],
+        "url": "https://roshameow.github.io//personal_homepage/docs/math/conformal-geometry5/"
+      },{
+        "title": "conformal geometry学习 (六 ) -- (Simplicial) cohomology group",
+        "excerpt":"","categories": ["docs","math"],
+        "tags": ["content"],
+        "url": "https://roshameow.github.io//personal_homepage/docs/math/conformal-geometry6/"
       }]
